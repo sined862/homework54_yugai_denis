6b702e2cd5e3b8be4bffe527d3757e0086436a49
Yugai Denis <yugai_denis@mail.ru>
2022-09-27 19:01:53 +0600

    Добавление навигации в шаблоны + Добавление пути products/ + Создание фикстуры

--- webproject/market/urls.py
+++ webproject/market/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', index_view, name='index'),
     path('product/<int:pk>', detail_view, name='product_detail'),
     path('categories/add', add_category_view, name='add_category'),
-    path('products/add', add_product_view, name='add_product')
+    path('products/add', add_product_view, name='add_product'),
+    path('products/', index_view, name='products')
 ]
