f7bcc9bdf9937406db48beef29f067139d0b4a83
Yugai Denis <yugai_denis@mail.ru>
2022-09-27 15:49:32 +0600

    Описание админки + создание модели Product + создание и применение миграции

--- webproject/market/admin.py
+++ webproject/market/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from market.models import Category, Product
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_filter = ('id', 'title', 'description')
+    search_fields = ('id', 'title', 'description')
+    fields = ('title', 'description')
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'category')
+    list_filter = ('id', 'title', 'description', 'created_at', 'category', 'image', 'price')
+    search_fields = ('id', 'title', 'description')
+    fields = ('title', 'description', 'category', 'image', 'price')
+
+admin.site.register(Product, ProductAdmin)
--- webproject/market/models.py
+++ webproject/market/models.py
@@ -9,3 +9,16 @@ class Category(models.Model):
 
     def __str__(self) -> str:
         return f'{self.title}, {self.description}'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(verbose_name='Категория', to='market.Category', null=False, blank=False, related_name='categories', on_delete=models.CASCADE)
+    title = models.CharField(verbose_name='Наименование', max_length=100, null=False, blank=False)
+    description = models.TextField(verbose_name='Описание', max_length=600, null=False, blank=False, default='')
+    created_at = models.DateTimeField(verbose_name='дата и время создания', auto_now_add=True)
+    price = models.DecimalField(verbose_name='Стоимость', max_digits=10, decimal_places=3, null=False, blank=False)
+    image = models.CharField(verbose_name='Картинка', max_length=200,  null=False, blank=False, default='no image')
+
+
+    def __str__(self) -> str:
+        return f'{self.title}, {self.category}, {self.price}'
